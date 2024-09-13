1867aeaef52f83cfdcb1e312d661c714e3f7fcad
AlexVCaron <alex.valcourt.caron@usherbrooke.ca>
2024-09-13 18:49:32 +0000

    port devcontainer

---
+++ .devcontainer/updateContentCommand.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+GIT_REMOTE=$(git remote get-url origin)
+CURRENT_BRANCH=
+# Get tracked remote branch associated to current branch (default to main)
+{
+    git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null &&
+    CURRENT_BRANCH=$(git rev-parse --abbrev-ref --symbolic-full-name HEAD 2>/dev/null)
+} || {
+    CURRENT_BRANCH="main"
+}
+
+cat <<EOF > $XDG_CONFIG_HOME/nf-neuro/.env
+# This file is used to store environment variables for the project.
+# It is sourced by the shell on startup of every terminals.
+
+export PROFILE=docker
+export NFCORE_MODULES_GIT_REMOTE=$GIT_REMOTE
+export NFCORE_MODULES_BRANCH=$CURRENT_BRANCH
+export NFCORE_SUBWORKFLOWS_GIT_REMOTE=$GIT_REMOTE
+export NFCORE_SUBWORKFLOWS_BRANCH=$CURRENT_BRANCH
+EOF
+
+poetry install --no-root
