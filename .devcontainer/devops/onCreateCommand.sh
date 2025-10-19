86beeb97512c369d678cdea8852b19710182ba16
Alex Valcourt Caron <alexvc91@gmail.com>
2025-10-19 03:23:03 +0000

    replace devcontainer base by python-slim, create user, devcontainer is not root anymore, still has docker in docker working

--- .devcontainer/devops/onCreateCommand.sh
+++ .devcontainer/devops/onCreateCommand.sh
@@ -10,10 +10,8 @@ mkdir -p $XDG_CONFIG_HOME/nf-neuro
 touch $XDG_CONFIG_HOME/nf-neuro/.env
 echo "source $XDG_CONFIG_HOME/nf-neuro/.env" >> ~/.bashrc
 
-mkdir -p /nf-test/bin
-cd /nf-test/bin/
 curl -fsSL https://code.askimed.com/install/nf-test | bash -s ${NFTEST_VERSION}
-echo "export PATH=$PATH:/nf-test/bin" >> ~/.bashrc
+mv nf-test /usr/local/bin/.
 
 # Setup GitHub Actions testing environment
 echo "🎭 Setting up GitHub Actions testing tools..."
